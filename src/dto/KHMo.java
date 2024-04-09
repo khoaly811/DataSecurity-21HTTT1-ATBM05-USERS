3509106c3cf013e4985f2ee2c627fb69f148ba97
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2024-04-09 12:54:38 +0700

    Create KHMo.java

---
+++ src/dto/KHMo.java
@@ -0,0 +1,87 @@
+package dto;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import java.time.LocalDate;
+import java.util.Date;
+
+// DTO for KHMO table
+public class KHMo {
+    private final SimpleStringProperty MAHP = new SimpleStringProperty();
+    private final SimpleIntegerProperty HOCKY = new SimpleIntegerProperty();
+    private final SimpleIntegerProperty NAM = new SimpleIntegerProperty();
+    private final SimpleStringProperty MACT = new SimpleStringProperty();
+    private final SimpleObjectProperty<LocalDate> NGAYBD = new SimpleObjectProperty<>();
+    
+    public KHMo(){
+
+    }
+
+    public KHMo(String MAHP, int HOCKY, int NAM, String MACT, Date NGAYBD){
+        this.MAHP.set(MAHP);
+        this.HOCKY.set(HOCKY);
+        this.NAM.set(NAM);
+        this.MACT.set(MACT);
+        this.NGAYBD.set(NGAYBD);
+    }
+
+    // Getters and setters
+    public String getMAHP(){
+        return MAHP.get();
+    }
+
+    public void setMAHP(String MAHP){
+        this.MAHP.set(MAHP);
+    }
+
+    public SimpleStringProperty MAHPproperty(){
+        return MAHP;
+    }
+
+    public int getHOCKY(){
+        return HOCKY.get();
+    }
+
+    public void setHOCKY(int HOCKY){
+        this.HOCKY.set(HOCKY);
+    }
+
+    public SimpleIntegerProperty HOCKYproperty(){
+        return HOCKY;
+    }
+
+    public int getNAM(){
+        return NAM.get();
+    }
+
+    public void setNAM(int NAM){
+        this.NAM.set(NAM);
+    }
+
+    public SimpleIntegerProperty NAMproperty(){
+        return NAM;
+    }
+
+    public String getMACT(){
+        return MACT.get();
+    }
+
+    public void setMACT(String MACT){
+        this.MACT.set(MACT);
+    }
+
+    public SimpleStringProperty MACTproperty(){
+        return MACT;
+    }
+
+    public LocalDate getNGAYBD(){
+        return NGAYBD.get;
+    }
+
+    public void setNGAYBD(Date NGAYBD){
+        this.NGAYBD.set(NGAYBD);
+    }
+
+    public SimpleObjectProperty<LocalDate> NGAYBDproperty(){
+        return NGAYBD;
+    }
+}
