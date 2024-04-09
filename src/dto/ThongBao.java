5d7f0da7fc8fa6733827694d31bdfbcba62c4651
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2024-04-09 12:54:43 +0700

    Create ThongBao.java

---
+++ src/dto/ThongBao.java
@@ -0,0 +1,58 @@
+package dto;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import java.time.LocalDate;
+import java.util.Date;
+
+//DTO for THONGBAO table
+public class ThongBao {
+    private final SimpleStringProperty MATB = new SimpleStringProperty();
+    private final SimpleStringProperty NOIDUNG= new SimpleStringProperty();
+    private final SimpleObjectProperty<LocalDate> THOIGIAN = new SimpleObjectProperty<>();
+
+    public ThongBao(){
+
+    }
+
+    public ThongBao(String MATB, String NOIDUNG, LocalDate THOIGIAN){
+        this.MATB.set(MATB);
+        this.NOIDUNG.set(NOIDUNG);
+        this.THOIGIAN.set(THOIGIAN);
+    }
+    // Getters and setters
+    public String getMATB(){
+        return MATB.get();
+    }
+
+    public void setMATB(String MATB){
+        this.MATB.set(MATB);
+    }
+
+    public SimpleStringProperty MATBproperty(){
+        return MATB;
+    }
+
+    public String getNOIDUNG(){
+        return NOIDUNG.get();
+    }
+
+    public void setNOIDUNG(String NOIDUNG){
+        this.NOIDUNG.set(NOIDUNG);
+    }
+
+    public SimpleStringProperty NOIDUNGproperty(){
+        return NOIDUNG;
+    }
+
+    public LocalDate getTHOIGIAN(){
+        return THOIGIAN.get();
+    }
+
+    public void setTHOIGIAN(LocalDate THOIGIAN){
+        this.THOIGIAN.set(THOIGIAN);
+    }
+
+    public SimpleObjectProperty<LocalDate> THOIGIANproperty(){
+        return THOIGIAN;
+    }
+}
