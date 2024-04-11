b7f448884fa842050b0aa951a0737557a7f831ae
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2024-04-11 21:17:14 +0700

    Update PHANCONGController.java

--- src/atbm05/PHANCONGController.java
+++ src/atbm05/PHANCONGController.java
@@ -1,5 +1,140 @@
 package atbm05;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+// import javafx.scene.control.TextField;
+import oracle.jdbc.OracleTypes;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import DataAccessLayer.DataAccessLayer;
+import dto.Phancong;
+import dto.Nhansu;
+import dto.Hocphan;
+
 public class PHANCONGController {
-    
+    @FXML
+    private TableView<Phancong> phancongTableView;
+
+    @FXML
+    private TableColumn<Phancong, String> HOTENGV;
+
+    @FXML
+    private TableColumn<Phancong, String> TENHP;
+
+    @FXML
+    private TableColumn<Phancong, String> HK;
+
+    @FXML
+    private TableColumn<Phancong, String> NAM;
+
+    @FXML
+    private TableColumn<Phancong, String> MACT;
+
+    @FXML
+    private void onAddClick_PHANCONG() {
+        System.out.println("Added");
+    }
+
+    @FXML
+    private void onUpdateClick_PHANCONG() {
+        System.out.println("Updated");
+    }
+
+    @FXML
+    private void onDeleteClick_PHANCONG() {
+        System.out.println("Delete");
+    }
+
+    // @FXML
+    // private TextField hotengvDisplay;
+
+    // @FXML
+    // private TextField tenhpDisplay;
+
+    // @FXML
+    // private TextField hockyDisplay;
+
+    // @FXML
+    // private TextField namDisplay;
+
+    // @FXML
+    // private TextField chuongtrinhDisplay;
+
+
+    private ObservableList<Phancong> phancongList = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        HOTENGV.setCellValueFactory(cellData -> cellData.getValue().getNhansu().HOTENproperty());
+        TENHP.setCellValueFactory(cellData -> cellData.getValue().getHocphan().TENHPproperty());
+        HK.setCellValueFactory(cellData -> cellData.getValue().HKproperty().asString());
+        NAM.setCellValueFactory(cellData -> cellData.getValue().NAMproperty().asString());
+        MACT.setCellValueFactory(cellData -> cellData.getValue().MACTproperty());
+        phancongList = FXCollections.observableArrayList();
+        loadPhancongFromDatabase();
+
+        // Add listener to the TableView selection model
+        // phancongTableView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+        //     if (newSelection != null) {
+        //         // Display the selected row's information in the corresponding TextFields
+        //         hotengvDisplay.setText(newSelection.getNhansu().getHOTEN());
+        //         tenhpDisplay.setText(newSelection.getHocphan().getTENHP());
+        //         hockyDisplay.setText(String.valueOf(newSelection.getHK()));
+        //         namDisplay.setText(String.valueOf(newSelection.getNAM()));
+        //         chuongtrinhDisplay.setText(newSelection.getMACT());
+        //     }
+        // });
+    }
+
+    private void loadPhancongFromDatabase() {
+        DataAccessLayer dal = null;
+        Connection conn = null;
+        CallableStatement cst = null;
+        ResultSet rs = null;
+
+        try {
+            dal = DataAccessLayer.getInstance("your_username", "your_password");
+            conn = dal.connect();
+            cst = conn.prepareCall("{CALL C##QLK.SP_VIEW_PHANCONG(?)}");
+            cst.registerOutParameter(1, OracleTypes.CURSOR);
+            System.out.println("nhan beo 1");
+            cst.execute();
+            System.out.println("nhan beo 2");
+            rs = (ResultSet) cst.getObject(1);
+            System.out.println("Nhan beo 3");
+            while (rs.next()) {
+                Phancong pc = new Phancong();
+                Nhansu ns = new Nhansu();
+                Hocphan hp = new Hocphan();
+
+                String tengv = rs.getString("HOTEN_GV");
+                System.out.println("TEN GIANG VIEN: " + tengv);
+                ns.setHOTEN(tengv);
+                pc.setNhansu(ns);
+
+                String tenhp = rs.getString("TEN_HP");
+                System.out.println("TEN HOC PHAN: " + tenhp);
+                hp.setTENHP(tenhp);
+                pc.setHocphan(hp);
+
+                pc.setHK((rs.getInt("HK")));
+                pc.setNAM((rs.getInt("NAM")));
+                pc.setMACT((rs.getString("MACT")));
+
+                phancongList.add(pc);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        }
+
+        phancongTableView.setItems(phancongList);
+    }
 }
