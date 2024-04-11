942266cbdb00d058a46cfce37330e93a864300f3
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2024-04-11 21:17:29 +0700

    Update KHMo.java

--- src/dto/KHMo.java
+++ src/dto/KHMo.java
@@ -2,40 +2,53 @@ package dto;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.ObjectProperty;
 import java.time.LocalDate;
 
 // DTO for KHMO table
 public class KHmo {
-    private final SimpleStringProperty MAHP = new SimpleStringProperty();
+    // private final SimpleStringProperty MAHP = new SimpleStringProperty();
     private final SimpleIntegerProperty HOCKY = new SimpleIntegerProperty();
     private final SimpleIntegerProperty NAM = new SimpleIntegerProperty();
     private final SimpleStringProperty MACT = new SimpleStringProperty();
     private final SimpleObjectProperty<LocalDate> NGAYBD = new SimpleObjectProperty<>();
-    
+    private final ObjectProperty<Hocphan> hocphanProperty = new SimpleObjectProperty<>();
     public KHmo(){
 
     }
 
-    public KHmo(String MAHP, int HOCKY, int NAM, String MACT, LocalDate NGAYBD){
-        this.MAHP.set(MAHP);
+    public KHmo(int HOCKY, int NAM, String MACT, LocalDate NGAYBD){
+        // this.MAHP.set(MAHP);
         this.HOCKY.set(HOCKY);
         this.NAM.set(NAM);
         this.MACT.set(MACT);
         this.NGAYBD.set(NGAYBD);
     }
 
+    public Hocphan getHocphan() {
+        return hocphanProperty.get();
+    }
+
+    public void setHocphan(Hocphan hocphan) {
+        this.hocphanProperty.set(hocphan);
+    }
+
+    public ObjectProperty<Hocphan> hocphanProperty() {
+        return hocphanProperty;
+    }
+
     // Getters and setters
-    public String getMAHP(){
-        return MAHP.get();
-    }
+    // public String getMAHP(){
+    //     return MAHP.get();
+    // }
 
-    public void setMAHP(String MAHP){
-        this.MAHP.set(MAHP);
-    }
+    // public void setMAHP(String MAHP){
+    //     this.MAHP.set(MAHP);
+    // }
 
-    public SimpleStringProperty MAHPproperty(){
-        return MAHP;
-    }
+    // public SimpleStringProperty MAHPproperty(){
+    //     return MAHP;
+    // }
 
     public int getHOCKY(){
         return HOCKY.get();
