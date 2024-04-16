3f2debf35dc6b4b26c5f99f0c73abff2d6f2ae7d
Albert de Nafta <treblaatfan1406@gmail.com>
2024-04-16 12:46:17 +0700

    Update PHANCONGController.java

--- src/atbm05/PHANCONGController.java
+++ src/atbm05/PHANCONGController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -13,6 +14,7 @@ import oracle.jdbc.OracleTypes;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -147,8 +149,78 @@ public class PHANCONGController {
 
     @FXML
     private void updatePCClick(ActionEvent event) {
-       
+       String TENHP = hocphanDisplay.getText().trim();
+       String TENGV = giaovienDisplay.getText().trim();
+       String MACT = chuongtrinhDisplay.getText().trim();
+       int HK = Integer.parseInt(hockyDisplay.getText().trim());
+       int NAM = Integer.parseInt(namDisplay.getText().trim());
+
+       DataAccessLayer dal = null;
+        Connection conn = null;
+        CallableStatement cst = null;
+       try {
+            dal = DataAccessLayer.getInstance("", "");
+            conn = dal.connect();
+            cst = conn.prepareCall("{CALL C##QLK.SP_ALL_UPDATE_PHANCONG(?,?,?,?,?)}");
+            cst.setString(1, TENGV);
+            System.out.println(TENGV);
+            System.out.println(TENHP);
+            cst.setString(2, TENHP);
+            cst.setInt(3, HK);
+            cst.setInt(4, NAM);
+            cst.setString(5, MACT);
+  
+            int rowsAffected = cst.executeUpdate();
+
+            
+            if (rowsAffected > 0) {
+                System.out.println("Update successfully.");
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Success");
+                alert.setHeaderText(null);
+                alert.setContentText("Cập nhật thành công!");
+                alert.showAndWait();
+
+            } else{
+                System.out.println("Update unsuccessfully.");
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Lỗi!");
+                alert.showAndWait();
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to Update: " + e.getMessage());
+            //showAlert(Alert.AlertType.ERROR, "Error", "Failed to Update user: " + e.getMessage());
+            if (e.getMessage().contains("ORA-01031")) {
+                System.out.println("No privileges (no grant).");
+                // Show an error alert
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Lỗi");
+                alert.showAndWait();
+            }
+            if (e.getErrorCode() == -20003) {
+                // Display error message in an alert box
+                showAlert(AlertType.ERROR, "Error", "Custom Error HP", e.getMessage());
+            }
+            if (e.getErrorCode() == -20002) {
+                // Display error message in an alert box
+                showAlert(AlertType.ERROR, "Error", "Custom Error NS", e.getMessage());
+            }
+            else{
+                System.out.println("Unexpected error");
+                // Show an error alert
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Lỗi khi chạy !!!");
+                alert.showAndWait();
+            }
     }
+}
 
 
     private void showAlert(Alert.AlertType alertType, String title, String message) {
@@ -165,4 +237,12 @@ public class PHANCONGController {
         loadPhancongFromDatabase();
         phancongTableView.refresh();
     }
+    // Method to show alert
+private void showAlert(AlertType alertType, String title, String headerText, String contentText) {
+    Alert alert = new Alert(alertType);
+    alert.setTitle(title);
+    alert.setHeaderText(headerText);
+    alert.setContentText(contentText);
+    alert.showAndWait();
+}
 }
